36bce331feb997390b4519c5c740284e8ffaf6fd
CN24 <codenation@CN24s-MacBook-Pro.local>
2020-02-04 16:33:19 +0000

    finished keycode

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const keyNum1 = document.getElementById("keyNum1")
+
+document.addEventListener("keypress", () => {
+    keyNum1.textContent = event.keyCode
+})
+
+//
 const keyType = document.getElementById("keyType")
 
 document.addEventListener("keypress", () => {
@@ -7,10 +14,12 @@ document.addEventListener("keypress", () => {
 //
 const keyNum = document.getElementById("keyNum")
 
+
 document.addEventListener("keypress", () => {
     keyNum.textContent = event.keyCode
 })
 
+//
 const keyName = document.getElementById("keyName")
 
 document.addEventListener("keypress", () => {
